37858fb80029080357f30de5e42dca15c564d754
Quentin Burke <Quentin.Burke@pbd.com>
2022-11-24 10:25:20 -0500

    Fixes

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle ');
 
-let currentActive = 1;
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
 
 next.addEventListener('click', () => {
-    currentActive++ 
+    currentActive++
 
-    if (currentActive > circles.length) {
-        currentActive = circles.length 
-    } 
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
 
     update()
-
 })
 
 prev.addEventListener('click', () => {
     currentActive--
 
-    if (currentActive < 1) {
+    if(currentActive < 1) {
         currentActive = 1
-    } 
+    }
 
     update()
-
 })
 
 function update() {
@@ -38,16 +37,14 @@ function update() {
 
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width = ((actives.length -1) / (circles.length -1)) * 100 + '%'
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    if(currentActive = 1) {
-        prev.Disabled = true
-    } else if (currentActive === circles.length) {
-        next.Disabled = true 
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
     } else {
-        prev.Disabled = false 
-        next.Disabled = false 
-    }  
+        prev.disabled = false
+        next.disabled = false
+    }
 }
-    
-
